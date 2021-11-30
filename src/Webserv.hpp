b6409a4b1a08ab3716fc81694c66c06a98023446
nlaurids <nlaurids@s19.be>
2021-11-30 16:01:54 +0100

    something

--- src/Webserv.hpp
+++ src/Webserv.hpp
@@ -10,11 +10,14 @@ class Webserv
 		Webserv(const Webserv &cpy);
 		Webserv &operator=(const Webserv &a);
 		void	setup();
+		void	server();
 	private:
 		Server	*_Server;
 		int		_server_fd_highest;
 		fd_set	_read_fd;
 		fd_set	_write_fd;
+		void	_fd_set_set();
+		void	_handle_fd_set();
 };
 
 #endif
